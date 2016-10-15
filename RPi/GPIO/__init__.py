cf4441c2dc68d78b2223365339921e662034544a
Ben Croston <ben@croston.org>
2016-10-15 19:17:04 +0100

    Fix code so it builds under PyPy (Gasper Zejn)

--- RPi/GPIO/__init__.py
+++ RPi/GPIO/__init__.py
@@ -22,4 +22,4 @@ SOFTWARE.
 
 from RPi._GPIO import *
 
-VERSION = '0.6.2'
+VERSION = '0.6.3a0'
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Topic :: System :: Hardware']
 
 setup(name             = 'RPi.GPIO',
-      version          = '0.6.2',
+      version          = '0.6.3a0',
       author           = 'Ben Croston',
       author_email     = 'ben@croston.org',
       description      = 'A module to control Raspberry Pi GPIO channels',
