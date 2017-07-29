friendlyarm/BakeBit

add support for neo2/neo plus2/m1 plus

Script/RPi.GPIO-0.5.11/source/cpuinfo.c
@@ -44,7 +44,10 @@ int get_rpi_info(rpi_info *info)
           strcmp(hardware, "BCM2709") == 0 ||
           strcmp(hardware, "BCM2835") == 0 ||
           strcmp(hardware, "sun8i") == 0 ||
-	  strcmp(hardware, "NANOPI2") == 0 ||
+          strcmp(hardware, "sun50iw2") == 0 ||
+          strcmp(hardware, "Allwinnersun50iw2Family") == 0 ||
+          strcmp(hardware, "Allwinnersun8iFamily") == 0 ||
+	       strcmp(hardware, "NANOPI2") == 0 ||
           strcmp(hardware, "BCM2836") == 0) {
          found = 1;
       }

auto3000/RPi.GPIO_NP

Signed-off-by: auto3000 <auto3000@users.noreply.github.com>

source/cpuinfo.c
@@ -40,7 +40,12 @@ int get_rpi_info(rpi_info *info)
    while(!feof(fp)) {
       fgets(buffer, sizeof(buffer), fp);
       sscanf(buffer, "Hardware	: %s", hardware);
-      if (strcmp(hardware, "BCM2708") == 0 ||
+      if (strcmp(hardware, "sun8i") == 0 ||
+          strcmp(hardware, "sun50iw2") == 0 ||
+          strcmp(hardware, "Allwinnersun50iw2Family") == 0 ||
+          strcmp(hardware, "Allwinnersun8iFamily") == 0 ||
+          strcmp(hardware, "NANOPI2") || 
+          strcmp(hardware, "BCM2708") == 0 ||
           strcmp(hardware, "BCM2709") == 0 ||
           strcmp(hardware, "BCM2835") == 0 ||
           strcmp(hardware, "BCM2836") == 0 ||